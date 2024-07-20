18bbfe8f231f5b56de4acd3109eb9f8f30fcc939
Desheye <aarealaafiaboyz@gmail.com>
2024-07-20 17:48:31 +0100

    mobile version phase two

--- client/src/Pages/BlogPage.js
+++ client/src/Pages/BlogPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../css/blogPage.css';
+//import '../css/blogPage.css';
+import '../css/blog_mobile.css';
 
 function BlogPage() {
   return (
@@ -8,7 +9,19 @@ function BlogPage() {
   >
       <div className="top-right-corner">
       </div>
-        <h1>Blog Page</h1>
+      <h1>Browse Categories</h1>
+      <div className='category'>
+        <div className='card'>Performing Acts</div>
+        <div className='card'>Song Writers</div>
+        <div className='card'>UI/UX Designers</div>
+        <div className='card'>Web Developers</div>
+        <div className='card'>Music Producers</div>
+        <div className='card'>Consultancy Services</div>
+        <div className='card'>Event Planners</div>
+        <div className='card'>Legal Services</div>
+        <div className='card'>Hospitality</div>
+        <div className='card'>Services</div>
+        </div>
       </div>
   );
 }
